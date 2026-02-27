1c56988a7af61dbaf519b204f066d81b90ab4069
Thomas Wallace <thomaswallace@Thomass-MacBook-Air.local>
2026-02-27 21:01:20 +0000

    favicon + document setup

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/LLS_L_Only_Colour.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/LLS_L_Only_Colour.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
